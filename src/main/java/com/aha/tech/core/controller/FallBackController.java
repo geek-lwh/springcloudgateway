81120dc8dad0ae961307c17a1b19b3eae5cc268b
luweihong <luweihong@ahaschool.com>
2019-03-13 13:57:33 +0800

    add fallback controller

---
+++ src/main/java/com/aha/tech/core/controller/FallBackController.java
@@ -0,0 +1,20 @@
+package com.aha.tech.core.controller;
+
+import com.aha.tech.commons.response.RpcResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: luweihong
+ * @Date: 2019/3/12
+ */
+@RestController
+public class FallBackController {
+
+    @RequestMapping(value = "/fallback")
+    public RpcResponse fallBack(){
+        RpcResponse rpcResponse = RpcResponse.defaultFailureResponse();
+        rpcResponse.setMessage("网关调用服务进入降级");
+        return rpcResponse;
+    }
+}
