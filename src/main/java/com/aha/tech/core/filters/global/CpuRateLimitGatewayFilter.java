efa46d88fb7c89e8263ee63e8f406b68a98ff5de
luweihong <luweihong@ahaschool.com>
2019-03-25 10:40:55 +0800

    cpu限流

--- src/main/java/com/aha/tech/core/filters/global/CpuRateLimitGatewayFilter.java
+++ src/main/java/com/aha/tech/core/filters/global/CpuRateLimitGatewayFilter.java
@@ -5,12 +5,14 @@ import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -27,7 +29,8 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.s
  * @Author: luweihong
  * @Date: 2019/3/19
  *
- * 判断当前的cpu使用率,进行限流
+ * 针对平均1分钟系统资源利用率进行限流
+ * 
  */
 @Component
 public class CpuRateLimitGatewayFilter implements GlobalFilter, Ordered {
@@ -37,13 +40,16 @@ public class CpuRateLimitGatewayFilter implements GlobalFilter, Ordered {
     @Autowired
     private MetricsEndpoint metricsEndpoint;
 
+    @Value("${system.load.average.1m.ratelimiter.enable:false}")
+    private Boolean isEnable;
+
+    @Value("${system.load.average.1m.ratelimiter.overload:0.85}")
+    private double systemLoadAverage;
+
     // java metrics 系统cpu指标
-    private static final String METRIC_NAME = "system.cpu.usage";
+    private static final String METRIC_NAME = "system.load.average.1m";
 
     private static final String CPU_OVERLOAD_ERROR_MSG = "cpu负载过高,限制访问";
-
-    // cpu使用率
-    private static final double MAX_USAGE = 0.850D;
 
     @Override
     public int getOrder() {
@@ -52,6 +58,10 @@ public class CpuRateLimitGatewayFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (!isEnable) {
+            return chain.filter(exchange);
+        }
+
         Double systemCpuUsage = metricsEndpoint.metric(METRIC_NAME, null)
                 .getMeasurements()
                 .stream()
@@ -61,12 +71,12 @@ public class CpuRateLimitGatewayFilter implements GlobalFilter, Ordered {
                 .filter(Double::isFinite)
                 .orElse(0.0D);
 
-        boolean ok = systemCpuUsage < MAX_USAGE;
+        boolean ok = systemCpuUsage < systemLoadAverage;
         if (ok) {
             return chain.filter(exchange);
         }
 
-        logger.error("cpu 负载过高当前值: {} 阈值 : {}", systemCpuUsage, MAX_USAGE);
+        logger.error("cpu 负载过高当前值: {} 平均1分钟系统利用率设定的阈值 : {}", systemCpuUsage, systemCpuUsage);
 
         return Mono.defer(() -> writeCpuOverloadError(exchange));
     }
@@ -79,6 +89,7 @@ public class CpuRateLimitGatewayFilter implements GlobalFilter, Ordered {
     private Mono<Void> writeCpuOverloadError(ServerWebExchange exchange) {
         setResponseStatus(exchange, HttpStatus.TOO_MANY_REQUESTS);
         final ServerHttpResponse resp = exchange.getResponse();
+        resp.getHeaders().setContentType(MediaType.APPLICATION_JSON_UTF8);
         RpcResponse rpcResponse = new RpcResponse(HttpStatus.TOO_MANY_REQUESTS.value(), CPU_OVERLOAD_ERROR_MSG);
         byte[] bytes = JSON.toJSONString(rpcResponse).getBytes(StandardCharsets.UTF_8);
         DataBuffer buffer = resp.bufferFactory().wrap(bytes);
