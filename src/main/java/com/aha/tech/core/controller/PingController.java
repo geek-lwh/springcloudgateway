220e12946bb93091187c1f5e61836d64748998cd
luweihong <luweihong@ahaschool.com>
2019-04-15 12:23:57 +0800

    增加ping接口

---
+++ src/main/java/com/aha/tech/core/controller/PingController.java
@@ -0,0 +1,18 @@
+package com.aha.tech.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: luweihong
+ * @Date: 2019/4/15
+ */
+@RestController
+public class PingController {
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public String ping() {
+        return "pong";
+    }
+}
