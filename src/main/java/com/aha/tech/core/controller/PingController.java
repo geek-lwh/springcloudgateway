c2cc5e7be1805c08389ebac6a06171ab7c46624c
luweihong <luweihong@ahaschool.com>
2020-04-30 10:28:14 +0800

    ping接口增加gatewayserver路径

--- src/main/java/com/aha/tech/core/controller/PingController.java
+++ src/main/java/com/aha/tech/core/controller/PingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
-    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @RequestMapping(value = "/gatewayserver/ping", method = RequestMethod.GET)
     public String ping() {
         return "pong";
     }
