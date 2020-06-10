a56957ad2d736f700775c743330e279b3c279dcd
luweihong <luweihong@ahaschool.com>
2020-06-10 14:07:20 +0800

    增加passport的traceId

--- src/main/java/com/aha/tech/core/controller/resource/PassportResource.java
+++ src/main/java/com/aha/tech/core/controller/resource/PassportResource.java
@@ -1,6 +1,7 @@
 package com.aha.tech.core.controller.resource;
 
 import com.aha.tech.core.controller.resource.fallback.PassportFallbackFactory;
+import com.aha.tech.core.interceptor.FeignRequestInterceptor;
 import com.aha.tech.passportserver.facade.api.AuthorizationFacade;
 import org.springframework.cloud.openfeign.FeignClient;
 
@@ -8,6 +9,6 @@ import org.springframework.cloud.openfeign.FeignClient;
  * @Author: luweihong
  * @Date: 2019/1/9
  */
-@FeignClient(name = "passportserver", path = "passportserver", fallbackFactory = PassportFallbackFactory.class)
+@FeignClient(name = "passportserver", path = "passportserver", fallbackFactory = PassportFallbackFactory.class,configuration = FeignRequestInterceptor.class)
 public interface PassportResource extends AuthorizationFacade {
 }
---
+++ src/main/java/com/aha/tech/core/interceptor/FeignRequestInterceptor.java
@@ -0,0 +1,38 @@
+package com.aha.tech.core.interceptor;
+
+import com.dianping.cat.Cat;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import static com.aha.tech.core.constant.HeaderFieldConstant.X_TRACE_ID;
+
+/**
+ * @Author: luweihong
+ * @Date: 2018/12/3
+ * <p>
+ * feign request 拦截器
+ * 用于动态传递header信息,如果有需要需要自己定义
+ */
+//@Component
+public class FeignRequestInterceptor implements RequestInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(FeignRequestInterceptor.class);
+
+    public static final String TRACE_ID = "traceId";
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String traceId = MDC.get(TRACE_ID);
+        if (StringUtils.isBlank(traceId)) {
+            traceId = Cat.createMessageId();
+        }
+
+        requestTemplate.header(X_TRACE_ID, traceId);
+    }
+
+
+}
